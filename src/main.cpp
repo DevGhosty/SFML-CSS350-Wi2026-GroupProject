8852911df5351817059467cd9b45ac64c311e352
Steven R <ghosty@Stevens-MacBook-Pro-3.local>
2026-02-09 15:00:05 -0800

    Replace pong-base with local Pong implementation

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,23 @@
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+	sf::RenderWindow window( sf::VideoMode( { 1000, 1000 } ), "Testing Window" );
+	sf::RectangleShape topPaddle;
+	topPaddle.setSize({120.f, 20.f});
+	sf::RectangleShape bottomPaddle;
+	bottomPaddle.setSize({120.f, 20.f});
+
+	topPaddle.setFillColor(sf::Color::White);
+	bottomPaddle.setFillColor(sf::Color::White);
+
+	sf::Vector2u windowSize = window.getSize();
+
+	topPaddle.setPosition((windowSize.x - topPaddle.getSize().x) / 2.f, 10.f);
+	bottomPaddle.setPosition((windowSize.x - bottomPaddle.getSize().x) / 2.f, windowSize.y - topPaddle.getSize().y - 10.f);
+
+
+
+
 
 	while ( window.isOpen() )
 	{
@@ -15,7 +29,6 @@ int main()
 		}
 
 		window.clear();
-		window.draw( shape );
 		window.display();
 	}
 }
